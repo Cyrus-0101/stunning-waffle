7e0dd279dfbb0b3e50ef7442a721cae77e855261
Toxic254 <cyrus_g@outlook.com>
2021-03-15 13:32:48 +0300

    Added pyqt5 to create a better calculator.

--- calc.py
+++ calc.py
@@ -1,105 +1,166 @@
-from tkinter import *
+#!/usr/bin/env python3
 
-win = Tk() # This is to create a basic window
-win.geometry("450x450")  # this is for the size of the window 
-win.resizable(0, 0)  # this is to prevent from resizing the window
-win.title("Calculator")
 
-###################Starting with functions ####################
-# 'btn_click' function : 
-# This Function continuously updates the 
-# input field whenever you enters a number
+"""PyCalc is a simple calculator built using Python and PyQt5."""
 
-def btn_click(item):
-    global expression
-    expression = expression + str(item)
-    input_text.set(expression)
+import sys
 
-# 'bt_clear' function :This is used to clear 
-# the input field
+from functools import partial
 
-def bt_clear(): 
-    global expression 
-    expression = "" 
-    input_text.set("")
- 
-# 'bt_equal':This method calculates the expression 
-# present in input field
- 
-def bt_equal():
-    global expression
-    result = str(eval(expression)) # 'eval':This function is used to evaluates the string expression directly
-    input_text.set(result)
-    expression = ""
- 
-expression = ""
- 
-# 'StringVar()' :It is used to get the instance of input field
- 
-input_text = StringVar()
- 
-# Let us creating a frame for the input field
- 
-input_frame = Frame(win, width=312, height=50, bd=0, highlightbackground="black", highlightcolor="black", highlightthickness=2)
- 
-input_frame.pack(side=TOP)
- 
-#Let us create a input field inside the 'Frame'
- 
-input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=50, bg="#eee", bd=0, justify=RIGHT)
- 
-input_field.grid(row=0, column=0)
- 
-input_field.pack(ipady=10) # 'ipady' is internal padding to increase the height of input field
- 
-#Let us creating another 'Frame' for the button below the 'input_frame'
- 
-btns_frame = Frame(win, width=312, height=272.5, bg="grey")
- 
-btns_frame.pack()
- 
-# first row
- 
-clear = Button(btns_frame, text = "C", fg = "black", width = 32, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_clear()).grid(row = 0, column = 0, columnspan = 3, padx = 1, pady = 1)
- 
-divide = Button(btns_frame, text = "/", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("/")).grid(row = 0, column = 3, padx = 1, pady = 1)
- 
-# second row
- 
-seven = Button(btns_frame, text = "7", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(7)).grid(row = 1, column = 0, padx = 1, pady = 1)
- 
-eight = Button(btns_frame, text = "8", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(8)).grid(row = 1, column = 1, padx = 1, pady = 1)
- 
-nine = Button(btns_frame, text = "9", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(9)).grid(row = 1, column = 2, padx = 1, pady = 1)
- 
-multiply = Button(btns_frame, text = "*", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("*")).grid(row = 1, column = 3, padx = 1, pady = 1)
- 
-# third row
- 
-four = Button(btns_frame, text = "4", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(4)).grid(row = 2, column = 0, padx = 1, pady = 1)
- 
-five = Button(btns_frame, text = "5", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(5)).grid(row = 2, column = 1, padx = 1, pady = 1)
- 
-six = Button(btns_frame, text = "6", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(6)).grid(row = 2, column = 2, padx = 1, pady = 1)
- 
-minus = Button(btns_frame, text = "-", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("-")).grid(row = 2, column = 3, padx = 1, pady = 1)
- 
-# fourth row
- 
-one = Button(btns_frame, text = "1", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(1)).grid(row = 3, column = 0, padx = 1, pady = 1)
- 
-two = Button(btns_frame, text = "2", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(2)).grid(row = 3, column = 1, padx = 1, pady = 1)
- 
-three = Button(btns_frame, text = "3", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(3)).grid(row = 3, column = 2, padx = 1, pady = 1)
- 
-plus = Button(btns_frame, text = "+", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("+")).grid(row = 3, column = 3, padx = 1, pady = 1)
- 
-# fourth row
- 
-zero = Button(btns_frame, text = "0", fg = "black", width = 21, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 4, column = 0, columnspan = 2, padx = 1, pady = 1)
- 
-point = Button(btns_frame, text = ".", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click(".")).grid(row = 4, column = 2, padx = 1, pady = 1)
- 
-equals = Button(btns_frame, text = "=", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_equal()).grid(row = 4, column = 3, padx = 1, pady = 1)
- 
-win.mainloop()
+# Import QApplication and the required widgets from PyQt5.QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QWidget
+
+__version__ = "0.1"
+__author__ = "Leodanis Pozo Ramos"
+
+ERROR_MSG = "ERROR"
+
+
+# Create a subclass of QMainWindow to setup the calculator's GUI
+class PyCalcUi(QMainWindow):
+    """PyCalc's View (GUI)."""
+
+    def __init__(self):
+        """View initializer."""
+        super().__init__()
+        # Set some main window's properties
+        self.setWindowTitle("PyCalc")
+        self.setFixedSize(235, 235)
+        # Set the central widget and the general layout
+        self.generalLayout = QVBoxLayout()
+        self._centralWidget = QWidget(self)
+        self.setCentralWidget(self._centralWidget)
+        self._centralWidget.setLayout(self.generalLayout)
+        # Create the display and the buttons
+        self._createDisplay()
+        self._createButtons()
+
+    def _createDisplay(self):
+        """Create the display."""
+        # Create the display widget
+        self.display = QLineEdit()
+        # Set some display's properties
+        self.display.setFixedHeight(35)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setReadOnly(True)
+        # Add the display to the general layout
+        self.generalLayout.addWidget(self.display)
+
+    def _createButtons(self):
+        """Create the buttons."""
+        self.buttons = {}
+        buttonsLayout = QGridLayout()
+        # Button text | position on the QGridLayout
+        buttons = {
+            "7": (0, 0),
+            "8": (0, 1),
+            "9": (0, 2),
+            "/": (0, 3),
+            "C": (0, 4),
+            "4": (1, 0),
+            "5": (1, 1),
+            "6": (1, 2),
+            "*": (1, 3),
+            "(": (1, 4),
+            "1": (2, 0),
+            "2": (2, 1),
+            "3": (2, 2),
+            "-": (2, 3),
+            ")": (2, 4),
+            "0": (3, 0),
+            "00": (3, 1),
+            ".": (3, 2),
+            "+": (3, 3),
+            "=": (3, 4),
+        }
+        # Create the buttons and add them to the grid layout
+        for btnText, pos in buttons.items():
+            self.buttons[btnText] = QPushButton(btnText)
+            self.buttons[btnText].setFixedSize(40, 40)
+            buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
+        # Add buttonsLayout to the general layout
+        self.generalLayout.addLayout(buttonsLayout)
+
+    def setDisplayText(self, text):
+        """Set display's text."""
+        self.display.setText(text)
+        self.display.setFocus()
+
+    def displayText(self):
+        """Get display's text."""
+        return self.display.text()
+
+    def clearDisplay(self):
+        """Clear the display."""
+        self.setDisplayText("")
+
+
+# Create a Model to handle the calculator's operation
+def evaluateExpression(expression):
+    """Evaluate an expression."""
+    try:
+        result = str(eval(expression, {}, {}))
+    except Exception:
+        result = ERROR_MSG
+
+    return result
+
+
+# Create a Controller class to connect the GUI and the model
+class PyCalcCtrl:
+    """PyCalc's Controller."""
+
+    def __init__(self, model, view):
+        """Controller initializer."""
+        self._evaluate = model
+        self._view = view
+        # Connect signals and slots
+        self._connectSignals()
+
+    def _calculateResult(self):
+        """Evaluate expressions."""
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDisplayText(result)
+
+    def _buildExpression(self, sub_exp):
+        """Build expression."""
+        if self._view.displayText() == ERROR_MSG:
+            self._view.clearDisplay()
+
+        expression = self._view.displayText() + sub_exp
+        self._view.setDisplayText(expression)
+
+    def _connectSignals(self):
+        """Connect signals and slots."""
+        for btnText, btn in self._view.buttons.items():
+            if btnText not in {"=", "C"}:
+                btn.clicked.connect(partial(self._buildExpression, btnText))
+
+        self._view.buttons["="].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttons["C"].clicked.connect(self._view.clearDisplay)
+
+
+# Client code
+def main():
+    """Main function."""
+    # Create an instance of `QApplication`
+    pycalc = QApplication(sys.argv)
+    # Show the calculator's GUI
+    view = PyCalcUi()
+    view.show()
+    # Create instances of the model and the controller
+    model = evaluateExpression
+    PyCalcCtrl(model=model, view=view)
+    # Execute calculator's main loop
+    sys.exit(pycalc.exec_())
+
+if __name__ == "__main__":
+    main()
